6a39b22d5955e231e1bf6f5b07d574fc1860216f
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-03-30 22:45:11 -0400

    initialize phaser and set it to listen for key-up events

--- rl.js
+++ rl.js
@@ -7,3 +7,26 @@ var COLS = 15;
 
 // number of actors per level, including player
 var ACTORS = 10;
+
+// initialize phaser, call create() once done
+var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
+  create: create
+});
+
+function create() {
+  // init keyboard commands
+  game.input.keyboard.addCallbacks(null, null, onKeyUp);
+}
+
+function onKeyUp(event) {
+  switch (event.KeyCode) {
+    case Keyboard.LEFT:
+
+    case Keyboard.RIGHT:
+
+    case Keyboard.UP:
+
+    case Keyboard.DOWN:
+      
+  }
+}
