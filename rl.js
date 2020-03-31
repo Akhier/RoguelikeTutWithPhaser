5274f6a13042632a32901d67c493175fb1576bc8
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2020-03-30 22:58:42 -0400

    call the initMap function from the create function

--- rl.js
+++ rl.js
@@ -17,6 +17,9 @@ var game = new Phaser.Game(COLS * FONT * 0.6, ROWS * FONT, Phaser.AUTO, null, {
 function create() {
   // init keyboard commands
   game.input.keyboard.addCallbacks(null, null, onKeyUp);
+
+  // initialize map
+  initMap();
 }
 
 function onKeyUp(event) {
@@ -36,7 +39,7 @@ function onKeyUp(event) {
 var map;
 
 function initMap() {
-  //create a new random map
+  // create a new random map
   map = [];
   for (let y = 0; y < ROWS; y++) {
     var newRow = [];
